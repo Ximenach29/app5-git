dafed35b860141fb46910297fafe311163badb9b
Ximena Contreras Hernandez <chimeena29@gmail.com>
2019-04-29 14:19:56 -0500

    3

--- js/acciones.js
+++ js/acciones.js
@@ -8,7 +8,7 @@ $(document).ready(function(e){
 		});//tap beep
 			
 				$('#vibrar').tap(function(){
-			navigator.notification.beep(5000);
+			navigator.notification.vibar(5000);
 		});//tap vibrar
 		
 	},false); //deviceready
